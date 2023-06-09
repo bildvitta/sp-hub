cd324bad91e7463f6b478af91a06ce2cc650c5fb
Guilherme Haynes Howe <zerossb@gmail.com>
2023-06-09 16:49:56 -0300

    Updated permissions on user

--- src/Console/Commands/Messages/Resources/Helpers/UserHelper.php
+++ src/Console/Commands/Messages/Resources/Helpers/UserHelper.php
@@ -3,6 +3,9 @@
 namespace BildVitta\SpHub\Console\Commands\Messages\Resources\Helpers;
 
 use BildVitta\SpHub\Models\HubCompany;
+use Cache;
+use Hash;
+use Spatie\Permission\Models\Permission;
 use stdClass;
 
 trait UserHelper
@@ -17,7 +20,7 @@ trait UserHelper
         if (!$user = $modelUser::withTrashed()->where('hub_uuid', $message->uuid)->first()) {
             $user = new $modelUser();
             $user->hub_uuid = $message->uuid;
-            $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+            $user->password = Hash::make('password');
         }
         $user->name = $message->name;
         $user->email = $message->email;
@@ -27,12 +30,68 @@ trait UserHelper
         $user->updated_at = $message->updated_at;
         $user->deleted_at = $message->deleted_at;
 
-        if (isset($message->is_superuser)) {
-            $user->company_id = $this->getCompanyId($message->company_uuid);
-            $user->is_superuser = $message->is_superuser;
-            $user->is_active = $message->is_active;
-        }
+        $user->company_id = $this->getCompanyId($message->company_uuid);
+        $user->is_superuser = $message->is_superuser;
+        $user->is_active = $message->is_active;
+
         $user->save();
+
+        if (config('app.slug')) {
+            $appSlug = config('app.slug');
+            $this->updatePermissions($user, $message->user_permissions->$appSlug);
+        }
+    }
+
+    private function updatePermissions($user, $userPermissions)
+    {
+        $userPermissions = (array) $userPermissions;
+        $permissionsArray = $this->userPermissionsToArray($userPermissions);
+
+        $this->clearPermissionsCache();
+
+        $localPermissions = Permission::toBase()->whereIn('name', $permissionsArray)
+            ->orderBy('name')->get('name')->pluck('name')->toArray();
+
+        $permissionsDiff = array_diff($permissionsArray, $localPermissions);
+        $permissionsInsert = [];
+
+        foreach ($permissionsDiff as $permission) {
+            $permissionsInsert[] = ['name' => $permission, 'guard_name' => 'web'];
+        }
+
+        if (!empty($permissionsInsert)) {
+            Permission::insert($permissionsInsert);
+        }
+
+        $userLocalPermissions = $user->permissions->pluck('name')->toArray();
+        $userPermissionsDiff = array_diff($permissionsArray, $userLocalPermissions);
+        $userLocalPermissionsDiff = array_diff($userLocalPermissions, $permissionsArray);
+
+        if (!empty($userPermissionsDiff) || !empty($userLocalPermissionsDiff)) {
+            $user->syncPermissions(...collect($permissionsArray)->toArray());
+            $user->refresh();
+        }
+    }
+
+    private function clearPermissionsCache()
+    {
+        $permissionCacheKey = config('permission.cache.key');
+        Cache::forget($permissionCacheKey);
+    }
+
+    private function userPermissionsToArray($userPermissions): array
+    {
+        $permissionsArray = [];
+        foreach ($userPermissions as $key => $value) {
+            if (!is_array($value)) {
+                $permissionsArray[] = "$key.$value";
+                continue;
+            }
+            foreach ($value as $array) {
+                $permissionsArray[] = "$key.$array";
+            }
+        }
+        return $permissionsArray;
     }
 
     /**
