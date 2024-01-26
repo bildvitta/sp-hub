5349710ccd3c45f306c78e4c26e84c12b6219d81
Fábio Martins <fabiohbmartins@gmail.com>
2024-01-26 17:41:05 -0300

    Addition of new fields in the messaging

---
+++ src/Console/Commands/Messages/Resources/Helpers/UserExtraFields.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BildVitta\SpHub\Console\Commands\Messages\Resources\Helpers;
+
+trait UserExtraFields
+{
+    /**
+     * @param array $fillable
+     * @return bool
+     */
+    protected function userHasExtraFields(array $fillable): bool
+    {
+        $extraFields = [
+            'document', 
+            'company_name', 
+            'address', 
+            'street_number', 
+            'complement',
+            'city',
+            'state',
+            'postal_code',
+        ];
+        foreach($extraFields as $extraField) {
+            if (!in_array($extraField, $fillable)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
