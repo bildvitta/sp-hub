dd832add8b03cd7aaac15812cfeb2c356f2bb593
Guilherme Haynes Howe <zerossb@gmail.com>
2026-03-09 16:36:29 -0300

    Enhance BrandImport and BrandsHelper to support main company UUID handling and update brand creation logic

--- src/Console/Commands/Messages/Resources/Helpers/BrandsHelper.php
+++ src/Console/Commands/Messages/Resources/Helpers/BrandsHelper.php
@@ -3,6 +3,7 @@
 namespace BildVitta\SpHub\Console\Commands\Messages\Resources\Helpers;
 
 use BildVitta\Hub\Entities\HubBrand;
+use BildVitta\SpHub\Models\HubCompany;
 
 trait BrandsHelper
 {
@@ -14,6 +15,16 @@ trait BrandsHelper
         }
 
         $brand->name = $message->name;
+
+        if (property_exists($message, 'main_company_uuid')) {
+            $brand->main_company_id = null;
+            if ($message->main_company_uuid) {
+                $brand->main_company_id = HubCompany::withTrashed()->where('uuid', $message->main_company_uuid)->value('id');
+            }
+        } else {
+            $brand->main_company_id = $message->main_company_id ?? null;
+        }
+
         $brand->created_at = $message->created_at;
         $brand->updated_at = $message->updated_at;
         $brand->deleted_at = $message->deleted_at;
